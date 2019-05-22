f4919f50963320224775cdc34ac0b28600e553c6
Hasa Elman <elman.hasa@ynap.com>
2019-05-23 00:49:17 +0200

    Clear local cache

--- service/index.js
+++ service/index.js
@@ -2,11 +2,14 @@
 const { spawnSync } = require('child_process');
 const fs = require('fs');
 const archive = require('./archiver');
-
+const path = require("path");
 exports.deploy = async (repoUrl, repoName) => {
 
     var tmpDir = '/tmp/' + repoName;
-
+    if (fs.existsSync(tmpDir)) {
+        rmDir(tmpDir);
+    }
+  
     await require('lambda-git')()
     const clone = spawnSync('git', ['clone', repoUrl], {
         cwd: '/tmp'
@@ -18,7 +21,24 @@ exports.deploy = async (repoUrl, repoName) => {
 
     logDirecotoryFiles(tmpDir);
 };
+function rmDir(dir) {
+    var list = fs.readdirSync(dir);
+    for(var i = 0; i < list.length; i++) {
+        var filename = path.join(dir, list[i]);
+        var stat = fs.statSync(filename);
 
+        if(filename == "." || filename == "..") {
+            // pass these files
+        } else if(stat.isDirectory()) {
+            // rmdir recursively
+            rmdir(filename);
+        } else {
+            // rm fiilename
+            fs.unlinkSync(filename);
+        }
+    }
+    fs.rmdirSync(dir);
+};
 function logDirecotoryFiles(dir) {
 
     console.log('searching for ' + dir);
